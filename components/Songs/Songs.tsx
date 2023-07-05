89e0eb514a214de34ad16394393969fe0ab233b6
Sweets Sweetman <sweetmantech@gmail.com>
2023-07-05 12:19:35 -0300

    I init <Songs />

---
+++ components/Songs/Songs.tsx
@@ -0,0 +1,5 @@
+const Songs = () => {
+  return <>HELLO WORLD</>
+}
+
+export default Songs
---
+++ lib/clients/alchemy.ts
@@ -0,0 +1,24 @@
+import { Alchemy, Network } from "alchemy-sdk";
+import getAlchemyNetwork from "../alchemy/getAlchemyNetwork";
+
+export const getAlchemy = (chainId: any) => {
+  const network = getAlchemyNetwork(chainId)
+
+  const config = {
+    apiKey: process.env.NEXT_PUBLIC_ALCHEMY_KEY,
+    network
+  };
+  const alchemy = new Alchemy(config);
+  return alchemy
+}
+
+export const alchemy = getAlchemy(
+  process.env.NEXT_PUBLIC_CHAIN_ID || "1"
+);
+
+const configLens = {
+  apiKey: process.env.NEXT_PUBLIC_ALCHEMY_KEY_POLYGON,
+  network: Network.MATIC_MAINNET,
+};
+
+export const alchemyLens = new Alchemy(configLens);
