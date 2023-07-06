22f741fd77008c1af8f2ad3b028995bf36981faf
Sweets Sweetman <sweetmantech@gmail.com>
2023-07-06 16:18:30 -0300

    I add query for all music onchain from connected wallet.

--- components/HomePage/HomePage.tsx
+++ components/HomePage/HomePage.tsx
@@ -1,14 +1,31 @@
 import { ConnectButton } from '@rainbow-me/rainbowkit'
+import { useEffect } from 'react'
+import { getNfts } from '../../lib/getNfts'
+import { useAccount, useNetwork } from 'wagmi'
 
-const HomePage = () => (
-  <div className="flex flex-col justify-center items-center h-[100vh] gap-11">
-    <div className="font-body">Onchain Playlist</div>
-    <div className="font-body">
-      Onchain Playlist turns any piece of music onchain into a smart wallet that can own
-      songs and interact with dApps across the Ethereum ecosystem.
+const HomePage = () => {
+  const { activeChain } = useNetwork()
+  const { data: account } = useAccount()
+
+  useEffect(() => {
+    const init = async () => {
+      const response = await getNfts(activeChain.id.toString(), account.address)
+      console.log('SWEETS RESPONSE', response)
+    }
+
+    init()
+  }, [activeChain, account])
+
+  return (
+    <div className="flex flex-col justify-center items-center h-[100vh] gap-11">
+      <div className="font-body">Onchain Playlist</div>
+      <div className="font-body">
+        Onchain Playlist turns any piece of music onchain into a smart wallet that can own
+        songs and interact with dApps across the Ethereum ecosystem.
+      </div>
+      <ConnectButton />
     </div>
-    <ConnectButton />
-  </div>
-)
+  )
+}
 
 export default HomePage
