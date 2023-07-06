c18c11c77ef789a5dd385155d235d50d63fd4538
Sweets Sweetman <sweetmantech@gmail.com>
2023-07-06 18:12:05 -0300

    I remove unused code from alchemy lib.

--- lib/clients/alchemy.ts
+++ lib/clients/alchemy.ts
@@ -1,5 +1,5 @@
 import getAlchemyNetwork from "../getAlchemyNetwork";
-import { Alchemy, Network } from "alchemy-sdk";
+import { Alchemy } from "alchemy-sdk";
 
 export const getAlchemy = (chainId: any) => {
   const network = getAlchemyNetwork(chainId)
@@ -11,14 +11,3 @@ export const getAlchemy = (chainId: any) => {
   const alchemy = new Alchemy(config);
   return alchemy
 }
-
-export const alchemy = getAlchemy(
-  process.env.NEXT_PUBLIC_CHAIN_ID || "1"
-);
-
-const configLens = {
-  apiKey: process.env.NEXT_PUBLIC_ALCHEMY_KEY_POLYGON,
-  network: Network.MATIC_MAINNET,
-};
-
-export const alchemyLens = new Alchemy(configLens);
