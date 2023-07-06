3db8cc58c51b0702e779485869d354325900fadf
Sweets Sweetman <sweetmantech@gmail.com>
2023-07-06 16:22:00 -0300

    I update Home Page text.

--- components/HomePage/HomePage.tsx
+++ components/HomePage/HomePage.tsx
@@ -9,21 +9,22 @@ const HomePage = () => {
 
   useEffect(() => {
     const init = async () => {
-      const response = await getNfts(activeChain.id.toString(), account.address)
+      const response = await getNfts(activeChain?.id?.toString(), account.address)
       console.log('SWEETS RESPONSE', response)
     }
 
+    if (!activeChain || !account) return
     init()
   }, [activeChain, account])
 
   return (
     <div className="flex flex-col justify-center items-center h-[100vh] gap-11">
-      <div className="font-body">Onchain Playlist</div>
-      <div className="font-body">
-        Onchain Playlist turns any piece of music onchain into a smart wallet that can own
-        songs and interact with dApps across the Ethereum ecosystem.
+      <div className="font-body">onchain CD</div>
+      <div className="font-body text-center">
+        turn any piece of music into a CD that can own songs and interact with dApps
+        across the Ethereum ecosystem.
       </div>
-      <ConnectButton />
+      <ConnectButton label="sign in" />
     </div>
   )
 }
