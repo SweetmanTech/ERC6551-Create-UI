6ccfe671ec8214d70837c64c5bd43c9f99ad00fd
Sweets Sweetman <sweetmantech@gmail.com>
2023-07-10 12:52:50 -0300

    I fix page showing wrong image.

--- components/Pages/HomePage/HomePage.tsx
+++ components/Pages/HomePage/HomePage.tsx
@@ -1,13 +1,13 @@
-import { useAccount } from 'wagmi'
+import { useAccount, useSigner } from 'wagmi'
 import CoverPage from '../CoverPage'
 import ConnectWalletPage from '../ConnectWalletPage'
 
 const HomePage = () => {
-  const { data: account } = useAccount()
-  console.log('SWEETS ACCOUNT', account)
+  const { data: signer } = useSigner()
+  console.log('SWEETS ACCOUNT', signer)
 
-  // return <>{account ? <CoverPage /> : <ConnectWalletPage />}</>
-  return <CoverPage />
+  return <>{signer ? <CoverPage /> : <ConnectWalletPage />}</>
+  // return <CoverPage />
 }
 
 export default HomePage
