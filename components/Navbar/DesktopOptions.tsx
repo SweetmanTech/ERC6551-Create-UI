8d60d76debcb50f49323d6b3fb905bd39bc681fc
Sweets Sweetman <sweetmantech@gmail.com>
2023-07-11 14:37:39 -0300

    I remove delta.

--- components/Navbar/DesktopOptions.tsx
+++ components/Navbar/DesktopOptions.tsx
@@ -1,15 +1,13 @@
 import { menuItems } from './menuItems'
 
-const DesktopOptions = () => {
-  return (
-    <div className="text-white flex gap-10">
-      {menuItems.map((navItem) => (
-        <a key={navItem.title} href={navItem.link} target="_blank" rel="noreferrer">
-          {navItem.title}
-        </a>
-      ))}
-    </div>
-  )
-}
+const DesktopOptions = () => (
+  <div className="text-white flex gap-10">
+    {menuItems.map((navItem) => (
+      <a key={navItem.title} href={navItem.link} target="_blank" rel="noreferrer">
+        {navItem.title}
+      </a>
+    ))}
+  </div>
+)
 
 export default DesktopOptions
