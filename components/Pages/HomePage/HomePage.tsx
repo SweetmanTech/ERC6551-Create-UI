b546e83e87746090ccf869b8694563e816b475b2
Sweets Sweetman <sweetmantech@gmail.com>
2023-07-11 12:46:44 -0300

    I remove unused components.

--- components/Pages/HomePage/HomePage.tsx
+++ components/Pages/HomePage/HomePage.tsx
@@ -4,10 +4,7 @@ import ConnectWalletPage from '../ConnectWalletPage'
 
 const HomePage = () => {
   const { data: signer } = useSigner()
-  console.log('SWEETS ACCOUNT', signer)
-
   return <>{signer ? <CoverPage /> : <ConnectWalletPage />}</>
-  // return <CoverPage />
 }
 
 export default HomePage
